5bdae0905dffb8fe32f732c6b08224e2436b4935
santakurow <newtakuproj0730@gmail.com>
2020-06-18 07:35:06 +0900

    create user model and testing rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
   get 'home/index'
+  get 'users/new'
   get "/*path", to: "home#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_06_17_214932) do
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+    t.string "nickname"
+    t.string "firstname"
+    t.string "lastname"
+    t.string "email"
+    t.text "pr"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+  end
 
 end
