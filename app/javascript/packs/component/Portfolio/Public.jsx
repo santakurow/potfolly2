1bafa21379fc9cb7064c2b59f66c79a00f6fc3d0
santakurow <newtakuproj0730@gmail.com>
2020-06-21 19:01:16 +0900

    Link import

--- app/javascript/packs/component/Portfolio/Public.jsx
+++ app/javascript/packs/component/Portfolio/Public.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Typography, Button, TextField, Paper } from '@material-ui/core'
 import { makeStyles } from "@material-ui/core/styles";
 import CameraAltIcon from "@material-ui/icons/CameraAlt";
+import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
